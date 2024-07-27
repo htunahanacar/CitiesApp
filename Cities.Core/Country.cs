daecb5312b909e6ff6cc4ac294c7e9884c6a290c
Tunahan <htacar528@gmail.com>
2024-07-28 00:01:31 +0300

    Create Country, City, CityDestination entities

---
+++ Cities.Core/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cities.Core
+{
+    public class Country : BaseEntity
+    {
+        public string Name { get; set; }
+        public string CountryCode { get; set; }
+        public string FlagUrl { get; set; }
+        public string Continent { get; set; }
+    }
+}
