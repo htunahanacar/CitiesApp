f8e610ed4166699e6ba0845a1193ec7d54c7d917
Tunahan <htacar528@gmail.com>
2024-07-28 16:26:58 +0300

    Create IService

---
+++ Cities.Core/IServices/IService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cities.Core.IServices
+{
+    public interface IService<T> where T: class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IQueryable<T>> GetAllAsync();
+        Task<IQueryable<T>> WhereAsync(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
