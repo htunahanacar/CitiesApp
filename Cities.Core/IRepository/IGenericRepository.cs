38ee021af9199ef1806c7fd6837800ec146ece4b
Tunahan <htacar528@gmail.com>
2024-07-28 16:11:45 +0300

    Create IGenericRepository

---
+++ Cities.Core/IRepository/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace Cities.Core.IRepository
+{
+    public interface IGenericRepository <T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IQueryable<T>> GetAll();
+        Task<IQueryable<T>> Where(Expression<Func<T, bool>> expression);
+        Task<bool> Any(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        void UpdateAsync(T entity);
+        void DeleteAsync(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
